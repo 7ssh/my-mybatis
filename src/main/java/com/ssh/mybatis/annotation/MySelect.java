d209d60521d04971416eb6b6a684b618c828f0a8
ssh <1318305209@qq.com>
2020-05-30 02:48:57 +0800

    自定义mybatis...

--- src/main/java/com/ssh/mybatis/annotation/MySelect.java
+++ src/main/java/com/ssh/mybatis/annotation/MySelect.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Select {
+public @interface MySelect {
 
     /**
      * 配置SQL语句的
